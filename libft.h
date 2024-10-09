974ff8572972f8920bff73ce6db6afaa466cc369
Matthieu Gaudin <mgaudin@1B6.42angouleme.fr>
2024-10-09 07:46:58 +0200

    first backup

---
+++ libft.h
@@ -0,0 +1,14 @@
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <stdio.h>
+
+int ft_isalpha(int c);
+int ft_isdigit(int c);
+int ft_isalnum(int c);
+int ft_isascii(int c);
+// int ft_toupper(int c);
+// int ft_tolower(int c);
+size_t ft_strlen(const char *s);
+
+#endif
---
+++ main.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    // printf("%ld", ft_strlen("test"));
+    // printf("%ld", ft_strlen(""));
+    //printf("%ld", ft_strlen(NULL));
+    // printf("%d", ft_isalpha('a'));
+    // printf("%d", ft_isalpha('Z'));
+    // printf("%d", ft_isalpha('0'));
+    // printf("%d", ft_isascii(0));
+    // printf("%d", ft_isascii(127));
+    // printf("%d", ft_isascii(234));
+    // printf("%d", ft_isalnum('a'));
+    // printf("%d", ft_isalnum('K'));
+    // printf("%d", ft_isalnum('0'));
+    // printf("%d", ft_isdigit('0'));
+    // printf("%d", ft_isdigit('9'));
+    // printf("%d", ft_isdigit('a'));
+}
