bb1ff1031378778ea3715676ecc7cf6a8ea5c019
mgaudin <matthieugaudin917@gmail.com>
2024-12-30 21:01:22 +0100

    ft_strcmp added

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mgaudin <mgaudin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:15:12 by mgaudin           #+#    #+#             */
-/*   Updated: 2024/10/16 19:08:16 by mgaudin          ###   ########.fr       */
+/*   Updated: 2024/12/30 21:00:40 by mgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(char *s1, char *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
